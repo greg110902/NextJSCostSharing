d94f1048a39e812a6d5845ad38b46e8c05cff67a
Greg <gwgh1g21@soton.ac.uk>
2024-08-28 15:41:01 +0100

    Added padding to buttons

--- app/components/navbar/NavBar.js
+++ app/components/navbar/NavBar.js
@@ -2,7 +2,7 @@ import NavButton from "./NavButton";
 
 export default function NavBar() {
   return (
-    <div className="flex bg-slate-800">
+    <div className="flex bg-slate-800 p-5">
       <NavButton name="Transactions" loc="/transactions" />
       <NavButton name="Create" loc="/create" />
     </div>
--- app/components/navbar/NavButton.js
+++ app/components/navbar/NavButton.js
@@ -1,7 +1,7 @@
 export default function NavButton({ name, loc }) {
   return (
     <div>
-      <a href={loc} className="text-white">
+      <a href={loc} className="text-white p-5">
         {name}
       </a>
     </div>
