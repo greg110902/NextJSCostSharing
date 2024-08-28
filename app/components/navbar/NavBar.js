39f5da222a999b7b8e7df6321b23e6c606cc8ebb
Greg <gwgh1g21@soton.ac.uk>
2024-08-28 15:38:13 +0100

    made navbar slate

--- app/components/navbar/NavBar.js
+++ app/components/navbar/NavBar.js
@@ -2,7 +2,7 @@ import NavButton from "./NavButton";
 
 export default function NavBar() {
   return (
-    <div>
+    <div className="flex bg-slate-800">
       <NavButton name="Transactions" loc="/transactions" />
       <NavButton name="Create" loc="/create" />
     </div>
