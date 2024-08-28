ca9156f96886249dbd30aa79007e134475d73a66
Greg <gwgh1g21@soton.ac.uk>
2024-08-28 15:34:09 +0100

    added button

--- app/components/navbar/NavBar.js
+++ app/components/navbar/NavBar.js
@@ -4,6 +4,7 @@ export default function NavBar() {
   return (
     <div>
       <NavButton name="Transactions" loc="/transactions" />
+      <NavButton name="Create" loc="/create" />
     </div>
   );
 }
