5e7bc642c4bbdb64bbfa2a139d73b2636a299412
Greg <gwgh1g21@soton.ac.uk>
2024-08-28 16:14:20 +0100

    text colours, removed inter.classname for bg-white

--- app/components/transactions/Card.js
+++ app/components/transactions/Card.js
@@ -7,12 +7,14 @@ export default function Card({
   date,
 }) {
   return (
-    <div className="card bg-slate-400 w-3/4 shadow-xl m-10 hover:bg-gray-100 hover:scale-105 flex justify-center">
+    <div className="card bg-slate-100 w-3/4 shadow-xl m-10 hover:bg-gray-100 hover:scale-105 flex justify-center">
       <div className="card-body">
-        <h2 className="card-title flex justify-center">{title}</h2>
-        <div>Submitted by: {author}</div>
-        <div>Amount: £{amount}</div>
-        <div>On: {date}</div>
+        <h2 className="card-title flex justify-center text-slate-900">
+          {title}
+        </h2>
+        <div className="text-slate-900">Submitted by: {author}</div>
+        <div className="text-slate-900">Amount: £{amount}</div>
+        <div className="text-slate-900">On: {date}</div>
       </div>
     </div>
   );
