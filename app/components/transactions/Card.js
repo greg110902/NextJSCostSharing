1d10fbfeae318f44caf9606bda00d9cadfbdaa13
Greg <gwgh1g21@soton.ac.uk>
2024-08-28 15:52:13 +0100

    added card component

---
+++ app/components/transactions/Card.js
@@ -0,0 +1,19 @@
+export default function Card({
+  transactionID,
+  author,
+  affected,
+  amount,
+  title,
+  date,
+}) {
+  return (
+    <div className="card bg-base-100 w-96 shadow-xl m-10 hover:bg-gray-100 hover:scale-105 w-3/4">
+      <div className="card-body">
+        <h2 className="card-title flex justify-center">{title}</h2>
+        <div>Submitted by: {author}</div>
+        <div>Amount: £{amount}</div>
+        <div>On: {date}</div>
+      </div>
+    </div>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,3 +1,15 @@
 import Image from "next/image";
+import Card from "./components/transactions/Card";
 
-export default function Home() {}
+export default function Home() {
+  return (
+    <Card
+      transactionID={1}
+      author={"Greg"}
+      affected={"Alivia"}
+      amount={100}
+      title={"Condoms"}
+      date={"28/08/2024"}
+    />
+  );
+}
