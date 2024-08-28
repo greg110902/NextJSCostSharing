12d9bcb8ac599caf8a6c6bca973a9030be38cc7f
Greg <gwgh1g21@soton.ac.uk>
2024-08-28 16:46:29 +0100

    added get transactions

--- app/page.js
+++ app/page.js
@@ -1,7 +1,30 @@
 import Image from "next/image";
 import Card from "./components/transactions/Card";
+import { useEffect, useState } from "react";
+
+function getTransactions() {}
 
 export default function Home() {
+  const [transactions, setTransactions] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchTransactions = async () => {
+      const { data } = await fetch(
+        "https://gettransactions.gwgh1g21.workers.dev/"
+      );
+      setTransactions(await data);
+      setLoading(false);
+    };
+    if (loading) {
+      fetchTransactions();
+    }
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
       <h1>Transactions</h1>
@@ -23,6 +46,7 @@ export default function Home() {
           date={"28/08/2024"}
         />
       </div>
+      <div>{JSON.stringify(transactions)}</div>
     </div>
   );
 }
