33f0079df4439b0baafb78cb9a8f18d9611c2908
Greg <gwgh1g21@soton.ac.uk>
2024-08-29 10:51:18 +0100

    Updated transaction modal

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import Card from "./components/transactions/Card";
 import { useEffect, useState } from "react";
 import supabase from "./utils/supabase";
 
-function getTransactions() {}
+function submitTransaction() {}
 
 export default function Home() {
   const [transactions, setTransactions] = useState([]);
@@ -29,23 +29,36 @@ export default function Home() {
   } else {
     return (
       <div>
-        <button
-          className="btn"
-          onClick={() => document.getElementById("my_modal_2").showModal()}
-        >
-          open modal
-        </button>
+        <div className="flex justify-center">
+          <button
+            className="btn flex justify-center m-5"
+            onClick={() => document.getElementById("my_modal_2").showModal()}
+          >
+            Add transaction
+          </button>
+        </div>
         <dialog id="my_modal_2" className="modal">
           <div className="modal-box">
-            <h3 className="font-bold text-lg">Hello!</h3>
-            <p className="py-4">Press ESC key or click outside to close</p>
+            <h3 className="font-bold text-lg">Add transaction</h3>
+            <form onSubmit={submitTransaction()}>
+              <label className="m-1">Author</label>
+              <input></input>
+              <label className="m-1">Title</label>
+              <input></input>
+              <label className="m-1">Amount</label>
+              <input type="number"></input>
+              <div className="flex justify-center">
+                <button className="flex justify-center">Submit</button>
+              </div>
+            </form>
+            <p className="py-4">Click outside to close</p>
           </div>
           <form method="dialog" className="modal-backdrop">
             <button>close</button>
           </form>
         </dialog>
         <h1>Transactions</h1>
-        <div>
+        <div className="flex justify-center">
           {transactions.map((transaction) => {
             return (
               <Card
