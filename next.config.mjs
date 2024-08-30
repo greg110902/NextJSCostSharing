52b32624b63a4ac114aac344be9bb5d984487532
Greg <gwgh1g21@soton.ac.uk>
2024-08-30 10:00:13 +0100

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const { EnvironmentPlugin, webpack } = require("webpack");
+const nextConfig = {
+  webpack(config) {
+    config.plugins.push(
+      new EnvironmentPlugin([
+        "NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY",
+        "CLERK_SECRET_KEY",
+      ])
+    );
+  },
+};
 
 export default nextConfig;
