a785cc1304b6d1fe9bfa787cb15e973f7475f1c2
Greg <gwgh1g21@soton.ac.uk>
2024-08-30 10:02:48 +0100

    tried process.env

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const { EnvironmentPlugin, webpack } = require("webpack");
-const nextConfig = {
-  webpack(config) {
-    config.plugins.push(
-      new EnvironmentPlugin([
-        "NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY",
-        "CLERK_SECRET_KEY",
-      ])
-    );
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
