70e5d57d025eccd7828102dda594516e68ab7cc0
Greg <gwgh1g21@soton.ac.uk>
2024-09-02 17:26:22 +0100

    changed text colours to black

--- app/components/userpage/card.js
+++ app/components/userpage/card.js
@@ -5,8 +5,8 @@ export default function Card({ text, amount }) {
   return (
     <div className="flex justify-center">
       <div className="align-middle border rounded-lg bg-slate-200 w-1/2 m-5 shadow-lg">
-        <div className="flex justify-center">{text}</div>
-        <div className="flex justify-center">{amount}</div>
+        <div className="flex justify-center text-black">{text}</div>
+        <div className="flex justify-center text-black">{amount}</div>
       </div>
     </div>
   );
