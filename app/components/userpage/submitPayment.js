135a230ae68528e138438029ca8c9a7cb29c21d8
Greg Garcia Humphreys <gwgh1g21@soton.ac.uk>
2024-09-06 00:40:00 +0100

    moved return out of condition

--- app/components/userpage/submitPayment.js
+++ app/components/userpage/submitPayment.js
@@ -22,54 +22,54 @@ export default function SubmitPayment({ user, users }) {
       onSubmit(user);
       location.reload();
     });
-
-    return (
-      <div className="my-4">
-        <div className="flex justify-center">
-          <label htmlFor="modal_1" className="btn">
-            Submit payment
-          </label>
-        </div>
-
-        <input type="checkbox" id="modal_1" className="modal-toggle" />
-        <div className="modal bg-slate-100" role="dialog">
-          <div className="modal-box bg-slate-100">
-            <h3 className="font-bold text-lg">Pay into house fund</h3>
-            <form id="transactionForm" onSubmit={() => onSubmit(user)}>
-              <div className="m-1">
-                <label className="m-1 text-black">Author</label>
-                <input
-                  id="author"
-                  disabled
-                  className="bg-slate-300 rounded flex flex-1 float-right"
-                  defaultValue={user.firstName}
-                ></input>
-              </div>
-              <div className="m-1">
-                <label className="m-1 text-black">Amount</label>
-                <input
-                  type="number"
-                  min={1}
-                  step={0.01}
-                  id="amount"
-                  className="bg-slate-300 text-black rounded float-right"
-                ></input>
-              </div>
-              <div className="modal-action">
-                <button
-                  className="btn"
-                  id="submit"
-                  htmlFor="modal_1"
-                  type="submit"
-                >
-                  Submit
-                </button>
-              </div>
-            </form>
-          </div>
-          <label className="modal-backdrop" htmlFor="modal_1"></label>
-        </div>
-      </div>
-    );
   }
+
+  return (
+    <div className="my-4">
+      <div className="flex justify-center">
+        <label htmlFor="modal_1" className="btn">
+          Submit payment
+        </label>
+      </div>
+
+      <input type="checkbox" id="modal_1" className="modal-toggle" />
+      <div className="modal bg-slate-100" role="dialog">
+        <div className="modal-box bg-slate-100">
+          <h3 className="font-bold text-lg">Pay into house fund</h3>
+          <form id="transactionForm" onSubmit={() => onSubmit(user)}>
+            <div className="m-1">
+              <label className="m-1 text-black">Author</label>
+              <input
+                id="author"
+                disabled
+                className="bg-slate-300 rounded flex flex-1 float-right"
+                defaultValue={user.firstName}
+              ></input>
+            </div>
+            <div className="m-1">
+              <label className="m-1 text-black">Amount</label>
+              <input
+                type="number"
+                min={1}
+                step={0.01}
+                id="amount"
+                className="bg-slate-300 text-black rounded float-right"
+              ></input>
+            </div>
+            <div className="modal-action">
+              <button
+                className="btn"
+                id="submit"
+                htmlFor="modal_1"
+                type="submit"
+              >
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+        <label className="modal-backdrop" htmlFor="modal_1"></label>
+      </div>
+    </div>
+  );
 }
