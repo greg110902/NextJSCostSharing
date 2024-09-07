a937f969f2f161b0adfff0645c689e961f52754e
Greg Garcia Humphreys <gwgh1g21@soton.ac.uk>
2024-09-07 14:07:59 +0100

    added comments to user queue page

--- app/users/[user]/queue/page.js
+++ app/users/[user]/queue/page.js
@@ -5,59 +5,77 @@ import supabase from "../../../utils/supabase";
 import Card from "../../../components/userpage/queue/Card";
 
 export default function QueuePage({ params }) {
+  // Initialise the payments, withdrawals and loading states.
+  // TODO: combine withdrawals and payments into one variable
   const [payments, setPayments] = useState([]);
   const [withdrawals, setWithdrawals] = useState([]);
   const [paymentsLoading, setPaymentsLoading] = useState(true);
   const [withdrawalsLoading, setWithdrawalsLoading] = useState(true);
 
+  // Get the user ID from the url slug
   const userID = params.user;
 
+  // Initialise database client
   const client = supabase();
 
+  // UseEffect runs on page refresh
   useEffect(() => {
     const fetchPayments = async () => {
+      // Select data from the database where it has been created by the user and is payment type
       const { data } = await client
         .from("payments")
         .select()
         .eq("author", userID)
         .eq("type", true);
 
+      // Set payments to the received array and set the payments loading to false
       setPayments(data);
       setPaymentsLoading(false);
     };
     const fetchWithdrawals = async () => {
+      // Select data from the database where it has been created by the user and is withdrawal type
       const { data } = await client
         .from("payments")
         .select()
         .eq("author", userID)
         .eq("type", false);
 
+      // Set withdrawals to the received array and set the withdrawals loading to false
       setWithdrawals(data);
       setWithdrawalsLoading(false);
     };
 
     if (withdrawalsLoading && paymentsLoading) {
+      // Run if the withdrawals and payments are loading.
       fetchPayments();
       fetchWithdrawals();
     }
   });
 
   if (!withdrawalsLoading && !paymentsLoading) {
+    // Run when the withdrawals and payments have finished loading.
+    // Initialise queue array
     let queue = [];
+
+    // Run if there are any payments by the author in the database
     if (payments != null) {
+      // Loop over the payments and add them to the queue.
       payments.forEach((payment) => {
         queue.push(payment);
       });
     }
 
+    // Run if there are any withdrawals by the author in the database
     if (withdrawals != null) {
       withdrawals.forEach((withdrawal) => {
+        // Loop over the withdrawals and add them to the queue.
         queue.push(withdrawal);
       });
     }
 
-    console.log("queue", queue);
-
+    // Sort the queue by date
+    // TODO: in future, remove this, sort when requesting from the database client
+    // using .order(...), and then use the type field to determine the type
     queue.sort((a, b) => {
       if (a.created_at < b.created_at) {
         return -1;
@@ -70,11 +88,14 @@ export default function QueuePage({ params }) {
 
     return (
       <div>
+        {/* Add a back to user button */}
         <a className="btn m-3" href={`/users/${userID}`}>
           Back
         </a>
         <div>
           <div className="flex flex-wrap justify-center ">
+            {/* Map the payments and withdrawals in the queue 
+            to cards. */}
             {queue.map((element) => {
               return (
                 <Card
@@ -92,6 +113,7 @@ export default function QueuePage({ params }) {
       </div>
     );
   } else {
+    // Return loading if the page is still loading
     return <div>Loading...</div>;
   }
 }
