3fb30ccd988fcdbc9c8153ea7045116373450b0b
Greg Garcia Humphreys <gwgh1g21@soton.ac.uk>
2024-09-07 15:26:05 +0100

    modified config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,19 @@
-const withPWA = require("next-pwa");
-
-module.exports = withPWA({
-  pwa: {
-    dest: "public",
-    swSrc: "service-worker.js",
-  },
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  sw: "service-worker.js",
+  disable: process.env.NODE_ENV === "development",
 });
+
+/** @type {import('next').NextConfig} */
+const path = require("path");
+
+const nextConfig = {
+  reactStrictMode: false,
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+};
+
+module.exports = withPWA(nextConfig);
