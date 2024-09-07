6bce1cce5e9245e5938c3a9dea57883ea2fe7188
Greg Garcia Humphreys <gwgh1g21@soton.ac.uk>
2024-09-07 13:55:42 +0100

    Added comments to admin page

--- app/admin/queue/page.js
+++ app/admin/queue/page.js
@@ -6,21 +6,27 @@ import Card from "../../components/admin/AdminQueueCard";
 import { useUser } from "@clerk/nextjs";
 
 export default function QueuePage() {
+  // Define the variable and loading states
   const [payments, setPayments] = useState([]);
   const [paymentsLoading, setPaymentsLoading] = useState(true);
   const [all, setAll] = useState(false);
 
+  // Initialise the user
   const { isSignedIn, user } = useUser();
 
+  // Initialise the database client
   const client = supabase();
 
+  // UseEffect runs on page reload
   useEffect(() => {
     const fetchPayments = async () => {
+      // Order and then select the payments
       const { data } = await client
         .from("payments")
         .select()
         .order("created_at", { ascending: false });
 
+      // Set the data state and set the loading to false
       setPayments(data);
       setPaymentsLoading(false);
     };
@@ -30,30 +36,40 @@ export default function QueuePage() {
     }
   });
 
+  // Initialise queue array
   let queue = [];
 
   const onChange = (e) => {
+    // When toggle is selected, change the variable which controls
+    // whether all payments are visible to the toggle value
     setAll(e.target.checked);
   };
 
-  console.log("QUEUE", queue);
+  // Run if the user is signed in and the user is an admin
   if (isSignedIn && user.publicMetadata.role === "admin") {
+    // Run if the payments have finished loading
     if (!paymentsLoading) {
+      // Run if the toggle is set to all
       if (all) {
+        // Loop over all of the payments and add to the queue
         payments.forEach((payment) => {
           queue.push(payment);
         });
       } else {
+        // If the toggle is not set to all, loop over all of the
+        // payments and add those which have new=true
         payments.forEach((payment) => {
           if (payment.new === true) {
             queue.push(payment);
           }
         });
       }
+      // Return the component
       return (
         <div className="flex flex-wrap justify-center ">
           <div className="flex">
             <label className="mx-5">Show all</label>
+            {/* Create the all/only new payments toggle */}
             <input
               type="checkbox"
               className="toggle"
@@ -62,6 +78,8 @@ export default function QueuePage() {
               }}
             />
           </div>
+
+          {/* Map over the payments in the queue. Generate a card for each. */}
           {queue.map((element) => {
             return (
               <Card
@@ -77,9 +95,15 @@ export default function QueuePage() {
         </div>
       );
     } else {
+      {
+        /* if the user is not an admin, return not allowed */
+      }
       return <>You do not have permission to view this page.</>;
     }
   } else {
+    {
+      /* If the page is still loading, return loading screen */
+    }
     return <div>Loading...</div>;
   }
 }
