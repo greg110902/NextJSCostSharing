5e19dd4b0710557e93e9226415056b49461e9b04
Greg Garcia Humphreys <gwgh1g21@soton.ac.uk>
2024-09-08 12:35:53 +0100

    removed onesignal stuff, replacing with npm package

--- app/components/navbar/Drawer.js
+++ app/components/navbar/Drawer.js
@@ -1,5 +1,3 @@
-"use client";
-
 import { UserButton } from "@clerk/nextjs";
 import { currentUser } from "@clerk/nextjs/server";
 import DetailsModal from "./detailsModal";
--- app/layout.js
+++ app/layout.js
@@ -1,32 +1,18 @@
-"use client";
-
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Drawer from "./components/navbar/Drawer";
 import { ClerkProvider, SignedIn, SignedOut } from "@clerk/nextjs";
 import { SignInButton, UserButton } from "@clerk/nextjs";
-import { useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
+export const metadata = {
+  manifest: "./manifest.json",
+  title: "54 BelRD",
+  description: "54 Belmont Road finances app.",
+};
+
 export default async function RootLayout({ children }) {
-  useEffect(() => {
-    window.OneSignal = window.OneSignal || [];
-    OneSignal.push(function () {
-      OneSignal.init({
-        appId: "b40b7cc7-13dc-4662-8b48-efa668f9b72a",
-        notifyButton: {
-          enable: true,
-        },
-
-        allowLocalhostAsSecureOrigin: true,
-      });
-    });
-
-    return () => {
-      window.OneSignal = undefined;
-    };
-  });
   return (
     <ClerkProvider
       publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
@@ -35,23 +21,6 @@ export default async function RootLayout({ children }) {
       <html lang="en" className="bg-white">
         <head>
           <link rel="manifest" href="./manifest.json" />
-          <script
-            src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js"
-            defer
-          ></script>
-          <script>
-            window.OneSignalDeferred = window.OneSignalDeferred || [];
-            OneSignalDeferred.push(async function(OneSignal){" "}
-            {await OneSignal.init({
-              appId: "69778e3f-6742-4ea4-9dd3-ea721a4e4158",
-              safari_web_id:
-                "web.onesignal.auto.428d294a-5ce2-44bb-bee0-dec3149a5564",
-              notifyButton: {
-                enable: true,
-              },
-            })}
-            );
-          </script>
         </head>
 
         <body className="bg-white">
