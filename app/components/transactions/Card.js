46450d78dc1a2de145f4da618769d73ddd87f712
Greg <gwgh1g21@soton.ac.uk>
2024-09-21 20:26:00 +0100

    deleted modal stuff

--- app/components/transactions/Card.js
+++ app/components/transactions/Card.js
@@ -1,8 +1,5 @@
-"use client";
-
 import { useEffect, useState } from "react";
 import supabase from "../../utils/supabase";
-import PayerForm from "../transaction/payerForm";
 
 function userIDToName(userID, users) {
   // Looks up the ID and returns their first name
@@ -44,186 +41,109 @@ export default function Card({
   }, []);
 
   return (
-    <div>
-      <div className="card bg-slate-100 w-3/4 shadow-xl m-10 hover:bg-gray-100 hover:scale-105 flex justify-center">
-        {/* Returns a DaisyUI card component which shows transaction details */}
-        <div className="card-body">
-          <div className="card-title flex justify-between">
-            <h2 className="text-slate-900">{title}</h2>
-            <div className="dropdown">
-              <div
+    <div className="card bg-slate-100 w-3/4 shadow-xl m-10 hover:bg-gray-100 hover:scale-105 flex justify-center">
+      {/* Returns a DaisyUI card component which shows transaction details */}
+      <div className="card-body">
+        <div className="card-title flex justify-between">
+          <h2 className="text-slate-900">{title}</h2>
+          <div className="dropdown">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn rounded-full m-1 bg-transparent"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
                 tabIndex={0}
                 role="button"
-                className="btn rounded-full m-1 bg-transparent"
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="size-6"
-                  tabIndex={0}
-                  role="button"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z"
-                  />
-                </svg>
-              </div>
-              <ul
-                tabIndex={0}
-                className="dropdown-content menu bg-transparent rounded-box z-[1] w-52 p-2 shadow"
-              >
-                <li>
-                  <a>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="size-6"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M3 3v1.5M3 21v-6m0 0 2.77-.693a9 9 0 0 1 6.208.682l.108.054a9 9 0 0 0 6.086.71l3.114-.732a48.524 48.524 0 0 1-.005-10.499l-3.11.732a9 9 0 0 1-6.085-.711l-.108-.054a9 9 0 0 0-6.208-.682L3 4.5M3 15V4.5"
-                      />
-                    </svg>
-                  </a>
-                </li>
-                <li>
-                  <a>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="size-6"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-                      />
-                    </svg>
-                  </a>
-                </li>
-                {userID === authorID ? (
-                  <li>
-                    <input
-                      type="checkbox"
-                      id="my_modal_7"
-                      className="modal-toggle"
-                    />
-                    <div className="modal bg-slate-100" role="dialog">
-                      <div className="modal-box bg-slate-100">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth={1.5}
-                          stroke="currentColor"
-                          className="size-6"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
-                          />
-                        </svg>
-                        <form
-                          id="transactionForm"
-                          onSubmit={(e) => {
-                            // Prevent the default action (because of iOS)
-                            e.preventDefault();
-                            //submitForm(everyoneChecked, user, users);
-                          }}
-                        >
-                          <div className="m-1">
-                            <label className="m-1 text-black">Author</label>
-                            <input
-                              id="author"
-                              disabled
-                              className="bg-slate-300 rounded flex flex-1 float-right"
-                              defaultValue={user.firstName}
-                            ></input>
-                          </div>
-                          <div className="m-1">
-                            <label className="m-1 text-black">Title</label>
-                            <input
-                              id="title"
-                              className="bg-slate-300 text-black rounded float-right"
-                            ></input>
-                          </div>
-                          <div className="m-1">
-                            <label className="m-1 text-black">Amount</label>
-                            <input
-                              type="number"
-                              step={0.01}
-                              id="amount"
-                              className="bg-slate-300 text-black rounded float-right"
-                            ></input>
-                          </div>
-                          <div className="m-1">
-                            <label className=" text-black">
-                              Everyone paying
-                            </label>
-                            <input
-                              type="checkbox"
-                              defaultChecked={everyoneChecked}
-                              onClick={() =>
-                                setEveryoneChecked(!everyoneChecked)
-                              }
-                              className="checkbox align-middle mx-3"
-                            />
-                            {/* Show an additional form if the user is selecting a subset of the house */}
-                            {!everyoneChecked ? (
-                              <PayerForm currentID={user.id} />
-                            ) : (
-                              <></>
-                            )}
-                          </div>
-                          <div className="modal-action">
-                            {/* Button submits the form and hides the modal. */}
-                            <button
-                              className="btn"
-                              type="submit"
-                              htmlFor="my_modal_7"
-                            >
-                              Submit
-                            </button>
-                          </div>
-                        </form>
-                      </div>
-                      {/* Hides the modal when someone clicks off of it */}
-                      <label
-                        className="modal-backdrop"
-                        htmlFor="my_modal_7"
-                      ></label>
-                    </div>
-                  </li>
-                ) : (
-                  <></>
-                )}
-              </ul>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z"
+                />
+              </svg>
             </div>
+            <ul
+              tabIndex={0}
+              className="dropdown-content menu bg-transparent rounded-box z-[1] w-52 p-2 shadow"
+            >
+              <li>
+                <a>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="size-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M3 3v1.5M3 21v-6m0 0 2.77-.693a9 9 0 0 1 6.208.682l.108.054a9 9 0 0 0 6.086.71l3.114-.732a48.524 48.524 0 0 1-.005-10.499l-3.11.732a9 9 0 0 1-6.085-.711l-.108-.054a9 9 0 0 0-6.208-.682L3 4.5M3 15V4.5"
+                    />
+                  </svg>
+                </a>
+              </li>
+              <li>
+                <a>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="size-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                    />
+                  </svg>
+                </a>
+              </li>
+              {userID === authorID ? (
+                <li>
+                  <a>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="size-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+                      />
+                    </svg>
+                  </a>
+                </li>
+              ) : (
+                <></>
+              )}
+            </ul>
           </div>
-          <div className="text-slate-900">Submitted by: {author}</div>
-          <div className="text-slate-900">Amount: £{amount}</div>
-          <div className="text-slate-900">
-            {" "}
-            Affecting:{" "}
-            {affected.map((person) => {
-              return " " + userIDToName(person, users);
-            })}
-          </div>
-          <div className="text-slate-900">On: {date}</div>
         </div>
+        <div className="text-slate-900">Submitted by: {author}</div>
+        <div className="text-slate-900">Amount: £{amount}</div>
+        <div className="text-slate-900">
+          {" "}
+          Affecting:{" "}
+          {affected.map((person) => {
+            return " " + userIDToName(person, users);
+          })}
+        </div>
+        <div className="text-slate-900">On: {date}</div>
       </div>
     </div>
   );
